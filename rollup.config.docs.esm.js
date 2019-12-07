2e657b138987f3baca946ffd5ca47350838d53b9
unknown <jijinlong@wallstreetcn.com>
2019-12-07 19:26:03 +0800

    bug fix

---
+++ rollup.config.docs.esm.js
@@ -0,0 +1,19 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+	input: 'src/index.js',
+	output: {
+		format: 'esm',
+		file: 'docs/index.esm.bundle.js'
+	},
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel({
+			extensions: [ '.js' ],
+			exclude: 'node_modules/**'
+		})
+	]
+}
