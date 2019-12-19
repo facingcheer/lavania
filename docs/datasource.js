4d691c3ed5e2978ca282699c5fd5c90d80ea4345
季金龙 <jijinlong@jijinlongdeMacBook-Pro.local>
2019-12-19 13:52:47 +0800

    change data

--- docs/datasource.js
+++ docs/datasource.js
@@ -1,7 +1,7 @@
 (async function(){
   var demo_data_addr = 'https://forexdata.wallstreetcn.com/kline?prod_code=nzdusd&candle_period=1&data_count=10&end_time=0&fields=time_stamp,open_px,close_px,high_px,low_px,ma5';
 
-  let queryAddr = 'https://api-ddc-wscn.xuangubao.cn/market/kline?prod_code=000001.SS&tick_count=500&period_type=86400&fields=tick_at%2Copen_px%2Cclose_px%2Chigh_px%2Clow_px%2Cturnover_volume%2Cturnover_value%2Caverage_px%2Cpx_change%2Cpx_change_rate%2Cavg_px%2Cma2'
+  let queryAddr = 'https://api-ddc-wscn.xuangubao.cn/market/kline?prod_code=000001.SS&tick_count=200&period_type=86400&fields=tick_at%2Copen_px%2Cclose_px%2Chigh_px%2Clow_px%2Cturnover_volume%2Cturnover_value%2Caverage_px%2Cpx_change%2Cpx_change_rate%2Cavg_px%2Cma2'
   
 
   const response = await fetch(queryAddr)
